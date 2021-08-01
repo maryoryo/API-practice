5623263dca01c03499b213b2f93ea0a6666597d7
maroyroy <liyomaxxde@gmail.com>
2021-08-01 20:36:24 +0000

    [create]20210801

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  
+  root to: 'homes#top'
+  
+  resources :posts
 end

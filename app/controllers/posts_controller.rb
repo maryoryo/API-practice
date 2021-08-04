73052c28c467be7df3dab71393c38d1f106c9b44
maroyroy <liyomaxxde@gmail.com>
2021-08-04 21:22:28 +0000

    [create]20210804

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,37 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
-  def index
-    @posts = Post.all
+  # # GET /posts or /posts.json
+  # def index
+  #   @posts = Post.all
+  # end
+  
+  
+  GOOGLE_API_KEY = Rails.application.credentials.google[:api_key]
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = GOOGLE_API_KEY
+
+    next_page_token = nil
+    opt = {
+      q: keyword,
+      type: 'video',
+      max_results: 3,
+      order: :date,
+      page_token: next_page_token,
+      published_after: after.iso8601,
+      published_before: before.iso8601
+    }
+    service.list_searches(:snippet, opt)
   end
+
+  def index
+    @youtube_data = find_videos('明日香ちゃんねる')
+  end
+end
+  
+  
 
   # GET /posts/1 or /posts/1.json
   def show
